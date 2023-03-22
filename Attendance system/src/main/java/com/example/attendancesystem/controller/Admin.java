fa78e354deb3dc93a37d28813dced1c3b7cd5928
Haj Rezvan <rezvan@gmail.com>
2023-03-22 14:10:25 +0430

    Implement addEvent() with DatabaseDriver class. Add showClasses() & delete() methods with DatabaseDriver class.

--- Attendance system/src/main/java/com/example/attendancesystem/controller/Admin.java
+++ Attendance system/src/main/java/com/example/attendancesystem/controller/Admin.java
@@ -1,12 +1,11 @@
 package com.example.attendancesystem.controller;
 
+import com.example.attendancesystem.database.DatabaseDriver;
 import com.example.attendancesystem.model.Event;
 
-import java.util.ArrayList;
 import java.util.Date;
 
-public class Admin extends User{
-
+public class Admin extends User {
 
     public Admin(String username, String password, String name, int role) {
         super(username, password, name, role);
@@ -14,6 +13,20 @@ public class Admin extends User{
 
     public int addEvent(String title, Date date) {
         Event event = new Event(title, date);
-        return 0;
+
+        // TODO Database driver for test Admin class
+        if (getDriver().insert(event)) return 0;
+        return -1;
+    }
+
+    public void showClasses() {
+        // TODO Database driver for test Admin class
+        System.out.println(getDriver().get());
+    }
+
+    public int delete(Event event) {
+        // TODO Database driver for test Admin class
+        if (getDriver().delete(event)) return 0;
+        return -1;
     }
 }
