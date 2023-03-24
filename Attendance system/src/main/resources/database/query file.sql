597db94030b5a7962f786f940d0f163a3756da30
Haj Rezvan <rezvan@gmail.com>
2023-03-25 03:53:48 +0430

    Refactor the project.

--- Attendance system/src/main/resources/database/query file.sql
+++ Attendance system/src/main/resources/database/query file.sql
@@ -3,7 +3,7 @@ CREATE TABLE users
     id       INTEGER PRIMARY KEY AUTO_INCREMENT,
     name     VARCHAR(255),
     username VARCHAR(255),
-    password VARCHAR(255),
+    password VARCHAR(256),
     role     int
 );
 
