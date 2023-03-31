352f0ff3a2bdf3b10538c1370c20513f4c286271
Haj Rezvan <rezvan@gmail.com>
2023-03-31 08:15:28 +0430

    Tests completed.

--- Attendance system/src/test/java/com/example/attendancesystem/Database/EventsDriverTest.java
+++ Attendance system/src/test/java/com/example/attendancesystem/Database/EventsDriverTest.java
@@ -18,35 +18,44 @@ public class EventsDriverTest {
     }
 
     @Test
-    public void testGet() {
-//        insertTest();
+    public void testGetAll() {
         ArrayList<Event> events = driver.getEvents();
-        System.out.println("Title\tDate\tMembers");
+        System.out.println("ID\tTitle\tDate\tMembers");
         for (Event event : events) {
-            System.out.println(event.getTitle() + "\t" + event.getDate() + "\t" + event.getMembers());
+            System.out.println(event.getId() + "\t" + event.getTitle() + "\t" + event.getDate() + "\t" + event.getMembers());
         }
+    }
+
+    @Test
+    public void testGet() {
+        Event event = driver.getEvent(5);
+        System.out.println("ID\tTitle\tDate\tMembers");
+        System.out.println(event.getId() + "\t" + event.getTitle() + "\t" + event.getDate() + "\t" + event.getMembers());
+
     }
 
     @Test
     public void insertTest() {
-        assertTrue(driver.insert(new Event("title", new Date(System.currentTimeMillis()))));
+        assertTrue(driver.insert(new Event("Class", new Date(System.currentTimeMillis()))));
     }
 
     @Test
     public void testUpdate() {
-        Event event = new Event("title", new Date(System.currentTimeMillis()));
         Event newEvent = new Event("Subject", new Date(System.currentTimeMillis()));
-        driver.insert(event);
-        assertTrue(driver.update(event, newEvent));
-        System.out.println("Update:\nTitle\tDate\tMembers");
-        for (Event ev : driver.getEvents()) {
-            System.out.println(ev.getTitle() + "\t" + ev.getDate() + "\t" + ev.getMembers().size());
-        }
+        ArrayList<String> member = new ArrayList<>();
+        member.add("User 1");
+        member.add("User 2");
+        member.add("User 3");
+        member.add("User 4");
+        member.add("User 5");
+        member.add("User 7");
+        newEvent.setMembers(member);
+        assertTrue(driver.update(5, newEvent));
     }
 
     @Test
     public void testDelete() {
         Event event = new Event("title", new Date(System.currentTimeMillis()));
-        if (driver.insert(event)) assertTrue(driver.delete(event));
+        if (driver.insert(event)) assertTrue(driver.delete(event.getId()));
     }
 }
--- Attendance system/src/test/java/com/example/attendancesystem/Database/UserDriveTest.java
+++ Attendance system/src/test/java/com/example/attendancesystem/Database/UserDriveTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 public class UserDriveTest {
 
@@ -16,11 +18,36 @@ public class UserDriveTest {
     }
 
     @Test
-    public void testGet() {
+    public void testAllGet() {
         ArrayList<User> users = drive.getUsers();
         for (User user :
                 users) {
-            System.out.println(user.getName());
+            System.out.println(user.getId() + "\t" + user.getName() + "\t" + user.getUsername() + "\t" + user.getHashMap().get(user.getUsername()) + "\t" + user.getRole());
         }
+    }
+
+    @Test
+    public void testGet() {
+        User user = drive.getUser(11);
+        System.out.println(user.getId() + "\t" + user.getName() + "\t" + user.getUsername() + "\t" + user.getHashMap().get(user.getUsername()) + "\t" + user.getRole());
+
+    }
+
+    @Test
+    public void testInsert() {
+        assertTrue(drive.insert(new User("username", "password", "name", 1)));
+    }
+
+
+    @Test
+    public void testUpdate() {
+        User user = new User("username", "password", "name", 1);
+        drive.insert(user);
+
+    }
+
+    @Test
+    public void testDelete() {
+        assertTrue(drive.delete(8));
     }
 }
