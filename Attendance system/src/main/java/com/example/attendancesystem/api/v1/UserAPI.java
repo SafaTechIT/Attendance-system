06edfd4be83244bb9639c9afc2ee781fd94a7834
Haj Rezvan <rezvan@gmail.com>
2023-04-11 14:24:27 +0430

    Remove additional library imports.

--- Attendance system/src/main/java/com/example/attendancesystem/api/v1/UserAPI.java
+++ Attendance system/src/main/java/com/example/attendancesystem/api/v1/UserAPI.java
@@ -8,8 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-
 @RestController
 @CrossOrigin(origins = "localhost:8080")
 @RequestMapping(produces = "application/json")
